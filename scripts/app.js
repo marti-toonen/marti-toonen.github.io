45dc971963ba14a970a0d638fa0898a35da1ad75
Marti <44981000+marti-toonen@users.noreply.github.com>
2021-01-22 17:37:24 -0500

    22.21

--- scripts/app.js
+++ scripts/app.js
@@ -9,10 +9,29 @@ for (var i = 0; i < images.length; i++) {
     modal.style.display = "block";
     modal_image.src = this.src;
   }
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
 }
 
-var span = document.getElementsByClassName("close")[0];
+// show function
 
-span.onclick = function() {
-  modal.style.display = "none";
+function togglesample(title) {
+    switch(title) {
+        case "br01":
+            var x = document.getElementById("br_sample01");
+            break;
+        case "br02":
+            var x = document.getElementById("br_sample02");
+            break;
+        case "h":
+            var x = document.getElementById("h_sample");
+            break;
+    }
+    if(x.style.display == "none")
+        x.style.display = "block";
+    else
+        x.style.display = "none";
 }
